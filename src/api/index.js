a092b1f1b03e538a9fdd995e203c381427de9b40
NirbhikN <nyoupane.nirbhik@gmail.com>
2022-04-27 17:21:38 +0545

    get restaurants

---
+++ src/api/index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
+
+export const getPlacesData= async(sw,ne)=>{
+    try {
+            const {data: { data }} = await axios.get(URL, {
+                params: {
+                bl_latitude: sw.lat,
+                tr_latitude: ne.lat,
+                bl_longitude: sw.lng ,
+                tr_longitude: ne.lng,
+                },
+                headers: {
+                "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+                "X-RapidAPI-Key":
+                "316b144b62mshf5bfb7a0da4f48fp1b7dd6jsn83b2f486bf17",
+                },
+            });
+
+        return data;
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+export default getPlacesData
